488acbddc900a146aa1d31ee12801dfad2a48608
Konstantin <sayk.tiger09@gmail.com>
2023-01-13 15:04:49 +0300

    add deskstop blocks hero,menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
 const sass        = require('gulp-sass')(require('sass'));
-const rename      = require("gulp-rename");
+const rename      = require('gulp-rename');
 const autoprefixer= require('gulp-autoprefixer');
 const cleanCSS    = require('gulp-clean-css');
 const imagemin    = require('gulp-imagemin');
 const htmlmin     = require('gulp-htmlmin');
-const svgSprite = require('gulp-svg-sprite');
+const svgSprite   = require('gulp-svg-sprite');
 // const imageminWebp = require('imagemin-webp');
 // const imageminw = require('imagemin');
 // const webp = require('gulp-webp');
@@ -78,7 +78,6 @@ gulp.task('watch', function(){
     gulp.watch("src/*.html").on("change",gulp.parallel(`html`));
     gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
     gulp.watch("src/fonts/**/*").on('all', gulp.parallel('fonts'));
-    gulp.watch("src/img/svgIcons/*").on('all', gulp.parallel('icons'));
     gulp.watch("src/img/**/*").on('all', gulp.parallel('images'));
 });
 
@@ -98,15 +97,6 @@ gulp.task(`fonts`,function(){
     return gulp.src("src/fonts/**/*")
         .pipe(gulp.dest("dist/fonts"))
         .pipe(browserSync.stream());
-});
-gulp.task(`icons`,function(){
-    return gulp.src("src/icons/**/*")
-        .pipe(gulp.dest("dist/icons"))
-        .pipe(browserSync.stream());
-});
-gulp.task(`mailer`,function(){
-    return gulp.src("src/mailer/**/*")
-        .pipe(gulp.dest("dist/mailer"));
 });
 
 gulp.task(`images`,function(){
@@ -134,4 +124,4 @@ gulp.task(`svgSprite`, function(){
 // });
 
 
-gulp.task('default', gulp.parallel('watch','server','styles',`scripts`,`fonts`,`icons`,`html`,`mailer`,`images`,`svgSprite`));
+gulp.task('default', gulp.parallel('watch','server','styles',`scripts`,`fonts`,`html`,`images`,`svgSprite`));
