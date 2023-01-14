848c0ba2f756fb3e5705bb9d44e71c82ea4e49a6
Konstantin <sayk.tiger09@gmail.com>
2023-01-14 21:28:30 +0300

    mobile version add

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,5 +2,37 @@
 document.addEventListener(`DOMContentLoaded`, () => {
 
 
-    
+    // Аккардион
+    (function () {
+        const accordions = document.querySelectorAll(".question__accordion");
+
+        const openAccordion = (accordion) => {
+            const content = accordion.querySelector(".question__accordion__content");
+            accordion.classList.add("question__accordion__active");
+            content.style.maxHeight = content.scrollHeight + "px";
+        };
+
+        const closeAccordion = (accordion) => {
+            const content = accordion.querySelector(".question__accordion__content");
+            accordion.classList.remove("question__accordion__active");
+            content.style.maxHeight = null;
+        };
+
+        accordions.forEach((accordion) => {
+            const intro = accordion.querySelector(".question__accordion__intro");
+            const content = accordion.querySelector(".question__accordion__content");
+
+            intro.onclick = () => {
+                if (content.style.maxHeight) {
+                    closeAccordion(accordion);
+                } else {
+                    accordions.forEach((accordion) => closeAccordion(accordion));
+                    openAccordion(accordion);
+                }
+            };
+        });
+    }());
+
+
+
 });
